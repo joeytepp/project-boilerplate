6c54753be58d9a303191aa909fca9d00236d9769
Joey <jmichaelt98@gmail.com>
2020-04-25 17:38:08 -0400

    Set up Typescript with webpacker

--- app/client/pages/Hello.tsx
+++ app/client/pages/Hello.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Hello() {
-  return <div>This is a custom hello message</div>;
+  return <div>This is a custom typescript message</div>;
 }
 
 export default Hello;
