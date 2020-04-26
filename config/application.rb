c9ded113d8d9e5a241dd612feec33f757057fa88
Joey <jmichaelt98@gmail.com>
2020-04-26 19:44:08 -0400

    Get rid of stylesheets and javascript for generators

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module ProjectBoilerplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
