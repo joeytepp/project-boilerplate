2622d176974e76028af7222db962e2b9bd915696
Joey <jmichaelt98@gmail.com>
2020-04-26 19:49:06 -0400

    Change pre-commit rubocop to only staged files

--- scripts/run-rubocop.bash
+++ scripts/run-rubocop.bash
@@ -4,5 +4,5 @@ set -e
 
 cd "${0%/*}/.."
 
-echo "Running rubocop"
-bundle exec rubocop
+echo 'Running rubocop on staged files'
+git diff --staged --name-only | xargs ls -1 2>/dev/null | grep \.rb$ | xargs bundle exec rubocop -a
