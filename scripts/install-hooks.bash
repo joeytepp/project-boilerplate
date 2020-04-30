461730850efaf2a71ee54b62bd5af76f0bee3aed
Joey <jmichaelt98@gmail.com>
2020-04-30 11:48:19 -0400

    Change schema dump to only include IDL

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -2,7 +2,5 @@
 
 GIT_DIR=$(git rev-parse --git-dir)
 
-echo "Installing git hooks..."
 cp ./scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
 chmod +x $GIT_DIR/hooks/pre-commit
-echo "Done!"
--- scripts/run-codegen.bash
+++ scripts/run-codegen.bash
@@ -5,7 +5,7 @@ set -e
 cd "${0%/*}/.."
 
 echo "Dumping graphql schema"
-rails graphql:schema:dump
+rails graphql:schema:idl
 
 echo "Running graphql code gen"
 yarn run codegen
